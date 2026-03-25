3833944b700a622cd43fdafe01e3d366a840abc7
grad_success_lab <gokulavasan640@gmail.com>
2026-03-25 08:43:47 +0530

    Delete python_projects directory

--- python_projects/boat_price_prediction/front_end/src/components/SectionHeader.js
+++
@@ -1,8 +0,0 @@
-import '../App.css';
-function SectionHeader({ title }) {
-    return (
-      <h2 className=" fw-bold text-center mb-4">{title}</h2>
-    );
-  }
-  export default SectionHeader;
-  
